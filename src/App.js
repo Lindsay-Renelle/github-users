51618fe0dac47e5828cef65194c457d053b5478e
Dubois Celine <celine.dubois17@free.fr>
2025-07-24 16:19:20 +0200

    Modification pour créer un composant (hook)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App () {
+  const [users, setUsers] = useState([]);
+
+useEffect(() => {
+  fetch("https://api.github.com/users")
+  .then((response) => response.json()) 
+  .then((data) => setUsers(data.slice(0, 4))) 
+  .catch((error) => console.error("Erreur :", error)); 
+}, []);
+
+return (
+  <div className= "App">
+    <header>
+      <h1>Github users</h1>
+    </header>
+
+    <main>
+      <div className="user-grid">
+        {users.map((user)=> (
+          <div key={user.id} className="user-card">
+            <img src={user.avatar_url} alt={user.login}/>
+            <h3>{user.login}</h3>
+            <a href={user.html_url} target="_blank" rel="noreferrer">
+              {user.html_url}
+            </a>
+          </div>
+        ))}
+      </div>
+    </main>
+  </div>
+);
 }
-
 export default App;
